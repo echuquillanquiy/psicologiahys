949ade787c3102d2a7f5b90de1dde0838babb536
echuquillanquiy <edward.axl3@gmail.com>
2024-01-18 16:50:32 -0500

    sumando campos preprando para validacion de resultados

--- resources/views/eysenck/create.blade.php
+++ resources/views/eysenck/create.blade.php
@@ -66,6 +66,24 @@
 
                                 @include('eysenck.includes.partial3')
 
+                                <div class="col-12 col-lg-2 col-sm-12 col-md-12 text-center">
+                                    <div class="mb-1">
+                                        <input type="text" id="totale" name="suma_e" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-12 col-lg-2 col-sm-12 col-md-12 text-center">
+                                    <div class="mb-1">
+                                        <input type="text" id="totaln" name="suma_n" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-12 col-lg-2 col-sm-12 col-md-12 text-center">
+                                    <div class="mb-1">
+                                        <input type="text" id="totall" name="suma_l" class="form-control">
+                                    </div>
+                                </div>
+
                                 <button type="submit" class="btn btn-primary btn-lg">ENVIAR RESPUESTAS</button>
 
                             </div>
@@ -79,5 +97,50 @@
 @endsection
 
 @section('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.e').on('change', function () {
+                // Inicializar la suma
+                var suma = 0;
 
+                // Iterar sobre todos los selects y sumar sus valores
+                $('.e').each(function () {
+                    suma += parseInt($(this).val()) || 0; // Convertir a entero y manejar posibles NaN
+                });
+
+                // Mostrar la suma en la vista
+                $('#totale').val(suma);
+
+            });
+
+            $('.n').on('change', function () {
+                // Inicializar la suma
+                var suma2 = 0;
+
+                // Iterar sobre todos los selects y sumar2 sus valores
+                $('.n').each(function () {
+                    suma2 += parseInt($(this).val()) || 0; // Convertir a entero y manejar posibles NaN
+                });
+
+                // Mostrar la suma2 en la vista
+                $('#totaln').val(suma2);
+
+            });
+
+            $('.l').on('change', function () {
+                // Inicializar la suma
+                var suma3 = 0;
+
+                // Iterar sobre todos los selects y sumar3 sus valores
+                $('.l').each(function () {
+                    suma3 += parseInt($(this).val()) || 0; // Convertir a entero y manejar posibles NaN
+                });
+
+                // Mostrar la suma3 en la vista
+                $('#totall').val(suma3);
+
+            });
+        });
+    </script>
 @endsection
